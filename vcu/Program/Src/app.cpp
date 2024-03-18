202cb8caef35fef704f03a635d8e8828c76a3022
NicoOhR <nimoohayonr@gmail.com>
2024-03-18 13:21:51 -0500

    skeleton Program folder

---
+++ vcu/Program/Src/app.cpp
@@ -0,0 +1,13 @@
+/*
+* (c) 2022 Dallas Formula Racing - Embedded Firmware Team
+* Formula SAE International Collegiate Chapter
+* GPL-3.0 License
+*/
+
+#include "app.hpp"
+
+void cppMain(){
+	while(1){
+
+	}
+}
